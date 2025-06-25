0b4d2d788c294a6045ac71052dd4581a1079b89c
RasonamentX <wmalexander9797@gmail.com>
2025-06-24 21:44:01 -0500

    #

--- clase1/alexander_morocho.py
+++ clase1/alexander_morocho.py
@@ -1,4 +1,4 @@
-#Ejercicio 1
+#Funciones Ejercicio 1
 
 def ejercicio1(peso, talla):
     #Cálculo IMC
@@ -13,21 +13,35 @@ def ejercicio1(peso, talla):
         mensaje = "Obesidad"
     return mensaje
 
-#Ejercicio 2
+#Funciones Ejercicio 2
 
 def numeros():
     #Pedido de números a comparar
-    a = input("Ingresa el número menor: ")
-    b = input("Ingresa el número mayor: ")
-    return int(a), int(b)
+    a = int(input("Ingresa el número menor: "))
+    b = int(input("Ingresa el número mayor: "))
+    return a, b
 
-def validacion():
+def entero_positivo(a, b):
     #Validar que son números enteros positivos
-    return type(a) == int and type(b) == int and a > 0 and b > 0
+    return a > 0 and b > 0
 
+def mayor_menor(a, b):
+    #Validar que el primer número sea menor que el segundo
+    return b > a
+
+def pares(a, b):
+    pares = []
+    for num in range(a, b + 1):
+        if num % 2 == 0:
+            pares.append(num)
+    print(f"Cantidad total de números en el intervalo: {b - a + 1}")
+    print(f"Números pares entre {a} y {b}: ")
+    for p in pares:
+        print(p)
+    return pares
 
 if __name__ == "__main__":
-    #Pedido de datos a usuario
+    #Ejercicio 1
     name = input("Nombre: ")
     edad = input("Edad: ")
     talla = float(input("Talla (m): "))
@@ -35,6 +49,14 @@ if __name__ == "__main__":
     
     msn = ejercicio1(peso, talla)
     print(f"Hola {name} tienes {edad} años y estás en: {msn}")
-
+    
+    
+    #Ejercicio 2
     a, b = numeros()
-    print(validacion())
+    
+    if not entero_positivo(a, b):
+        print("Los números deben ser enteros positivos")
+    elif not mayor_menor(a, b):
+        print("El primer número debe ser mayor al segundo número")
+    else:
+        pares(a, b)
